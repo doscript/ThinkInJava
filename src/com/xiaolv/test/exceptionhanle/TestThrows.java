eaf42badd2373ca28b2a262b9cdb0805a45796e8
Charlie Chen <doscript@126.com>
2017-04-24 18:32:32 +0800

    1. Runtime Exception 可以不用 throws 2. 重载或实现的函数不能抛出父类声明中的throws 定义之外的异常 ，即使这个异常是定义异常的父类也不行。

---
+++ src/com/xiaolv/test/exceptionhanle/TestThrows.java
@@ -0,0 +1,14 @@
+package com.xiaolv.test.exceptionhanle;
+
+import java.io.IOException;
+
+public class TestThrows {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		throw new Exception("authorization is not existed in header");
+
+	}
+
+}
