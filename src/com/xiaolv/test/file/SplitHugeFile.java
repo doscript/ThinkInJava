e5a62356038ce8a0ea1a3733a8704cd27210f947
doscript <doscript@126.com>
2018-06-12 16:17:09 +0800

    修改成java1.6用于测试在正式云服务器上的中文sql查询

--- src/com/xiaolv/test/file/SplitHugeFile.java
+++ src/com/xiaolv/test/file/SplitHugeFile.java
@@ -15,22 +15,23 @@ class SplitHugeFile {
         int sizeOfFiles = pieceSize;
         byte[] buffer = new byte[sizeOfFiles];
 
-        try (BufferedInputStream bis = new BufferedInputStream( new FileInputStream(f))) {//try-with-resources to ensure closing stream
-        	;
-            String name = f.getName();
-
-            int tmp = 0;
-            while ((tmp = bis.read(buffer)) > 0) {
-                //write each chunk of data into separate file with different number in name
-                File newFile = new File(f.getParent(), name + "."
-                        + String.format("%03d", partCounter++));
-                try (FileOutputStream out = new FileOutputStream(newFile)){
-                	;
-                    out.write(buffer, 0, tmp);//tmp is chunk size
-                }
-                
-            }
-        }
+        //java 1.7
+//        try (BufferedInputStream bis = new BufferedInputStream( new FileInputStream(f))) {//try-with-resources to ensure closing stream
+//        	;
+//            String name = f.getName();
+//
+//            int tmp = 0;
+//            while ((tmp = bis.read(buffer)) > 0) {
+//                //write each chunk of data into separate file with different number in name
+//                File newFile = new File(f.getParent(), name + "."
+//                        + String.format("%03d", partCounter++));
+//                try (FileOutputStream out = new FileOutputStream(newFile)){
+//                	;
+//                    out.write(buffer, 0, tmp);//tmp is chunk size
+//                }
+//                
+//            }
+//        }
     }
 
     public static void main(String[] args) throws IOException {
